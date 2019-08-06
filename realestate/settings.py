c83fec1a38ed57fb4d6f15f3fa60e05ac9ad446d
RonZhang724 <ronzhang@utexas.edu>
2019-08-06 09:52:01 -0500

    commit after awhile

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rondb',
+        'USER': 'postgres',
+        'PASSWORD': 'Ron724724',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
+
+# Media folder settiings 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
