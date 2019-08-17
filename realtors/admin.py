1dbbeca1ab2901804ac416536ed22e7342b78141
RonZhang724 <ronzhang@utexas.edu>
2019-08-17 13:31:02 -0500

    added new features

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from .models import Realtor 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'email', 'hire_date', 'is_mvp')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_per_page = 25
+    list_editable = ('is_mvp',)   # allow modifying the is_published
 
 admin.site.register(Realtor, RealtorAdmin)
